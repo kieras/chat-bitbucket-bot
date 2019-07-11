69ac8e362171943a716cfb828018c30e6522ea75
Kieras <akieras@ciandt.com>
2019-07-11 12:08:35 -0300

    Adding new interesting events paylod.

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,13 +17,13 @@ def load_fixture(filename):
 
 def test_pull_request_created(app):
 
-    # X-Request-UUID	afe23a8c-dde6-4cde-8eaa-3e50077849f4
-    # X-Event-Key	pullrequest: created
-    # X-Event-Time	Wed, 10 Jul 2019 20: 23: 28 GMT
-    # X-Attempt-Number	1
-    # X-Hook-UUID	fee8a257-2939-4b3a-aa64-2e07be1a8fb8
-    # User-Agent	Bitbucket-Webhooks/2.0
-    # Content-Type	application/json
+# X-Request-UUID	afe23a8c-dde6-4cde-8eaa-3e50077849f4
+# X-Event-Key	pullrequest: created
+# X-Event-Time	Wed, 10 Jul 2019 20: 23: 28 GMT
+# X-Attempt-Number	1
+# X-Hook-UUID	fee8a257-2939-4b3a-aa64-2e07be1a8fb8
+# User-Agent	Bitbucket-Webhooks/2.0
+# Content-Type	application/json
 
     data = load_fixture('./tests/fixtures/pullrequest-created.json')
     with app.test_request_context(method='POST', json=data):
@@ -62,3 +62,34 @@ def test_pull_request_rejected(app):
         res = main(flask.request)
         assert 'pullrequest' in res
 
+
+def test_commit_status_updated_successful(app):
+
+# X-Request-UUID	01e7f365-6430-4a79-bd5a-976acc8e228e
+# X-Event-Key	repo: commit_status_updated
+# X-Event-Time	Thu, 11 Jul 2019 15: 01: 11 GMT
+# X-Attempt-Number	1
+# X-Hook-UUID	fee8a257-2939-4b3a-aa64-2e07be1a8fb8
+# User-Agent	Bitbucket-Webhooks/2.0
+# Content-Type	application/json
+
+    data = load_fixture('./tests/fixtures/commit-status-updated-successful.json')
+    with app.test_request_context(method='POST', json=data):
+        res = main(flask.request)
+        assert 'SUCCESSFUL' in res
+
+
+def test_commit_status_updated_failed(app):
+
+# X-Request-UUID	1450daa7-5036-4b25-b24d-13fe76363b25
+# X-Event-Key	repo: commit_status_updated
+# X-Event-Time	Thu, 11 Jul 2019 14: 36:20 GMT
+# X-Attempt-Number	1
+# X-Hook-UUID	fee8a257-2939-4b3a-aa64-2e07be1a8fb8
+# User-Agent	Bitbucket-Webhooks/2.0
+# Content-Type	application/json
+
+    data = load_fixture('./tests/fixtures/commit-status-updated-failed.json')
+    with app.test_request_context(method='POST', json=data):
+        res = main(flask.request)
+        assert 'FAILED' in res
